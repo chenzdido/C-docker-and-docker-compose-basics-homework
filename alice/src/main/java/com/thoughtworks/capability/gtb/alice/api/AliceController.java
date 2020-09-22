f9ef24f5ee75668ba1efad897d972eca7fedbd87
chenzdido <chenzdido@gmail.com>
2020-09-22 23:04:55 +0800

    [zhe] feat:add /hello controller.

---
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/api/AliceController.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.capability.gtb.alice.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class AliceController {
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/hello")
+    public String hello() {
+       return restTemplate.getForEntity("http://localhost:8081/hello",String.class).getBody();
+    }
+}
